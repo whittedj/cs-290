606fba97a0e57985ff3abb46cc9c42254b777db7
whittedj <whittedj@github.com>
2021-01-11 01:57:55 -0500

    Create Goodbye.py
    Test Python file for HW1

---
+++ Goodbye.py
@@ -0,0 +1,8 @@
+# Description: This program is used to say Goodbye to the user.
+
+statement_1 = "Goodbye, "
+period = ". "
+statement_2 = "It was nice talking to you!"
+
+name = input("Please enter your name: ")
+print(statement_1 + name + period + statement_2)
